90ddfbed33bc2bf4159c17e7c3b9fda501951b16
Kals002 <Kuatpch02@>
2025-02-14 09:55:28 +0530

    Initial commit

--- Test1/src/main/java/org/example/Main.java
+++ Test1/src/main/java/org/example/Main.java
@@ -9,6 +9,9 @@ public class Main
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+        //Added a new line
+        System.out.println("Updated file");
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
